d99a513428852024299104db3cc4d63cf7b0b892
rai <ravaen1001@gmail.com>
2023-03-22 00:07:02 -0500

    generando archivos

--- automatizacion.py
+++ automatizacion.py
@@ -5,3 +5,19 @@ import pandas as pd
 doc = DocxTemplate("notificacion.docx")
 
 #carga de datos dinamicos, desde el excel
+
+df = pd.read_excel("calculocnr.xlsx", sheet_name="informacion")
+
+for index, fila in df.iterrows():
+    context = {'NOMBRE_USUARIO': fila['NOMBRE'],
+               'DIRECCION': fila['DIRECCION'],
+               'IMPORTE': fila['IMPORT'],
+               'CNR': fila['CNR'],
+               'NUMERACION': fila['NUMERACION'],
+               'NIS': fila['CODSUM']
+    }
+
+    context.update(my_context)
+
+    doc.render(context)
+    doc.save(f"doc_generado_{index}.docx")
